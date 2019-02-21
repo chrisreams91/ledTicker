f04faa6258079c462f6b03f7322417923e5bbdbe
Chris <chrisreams91@gmail.com>
2019-02-20 21:16:29 -0600

    oops

--- src/main.rs
+++ src/main.rs
@@ -31,21 +31,31 @@ fn display_text(duration: String) -> &'static str {
 }
 
 fn main() {
-    led_matrix::create_matrix_options();
     println!("Server started");
     rocket::ignite()
         .mount("/", routes![display_image, display_text])
         .launch();
 }
 
-// sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo -t 20 --led-rows=16 --led-chain=3 --led-show-refresh --led-pwm-lsb-nanoseconds 100 -D 1 /home/pi/images/dogeegod.ppm
+// sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo -t 20 --led-rows=16 --led-chain=3 --led-show-refresh --led-slowdown-gpio=2 --led-pwm-lsb-nanoseconds 100 -D 1 /home/pi/images/dogeegod.ppm
 
-// sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo -t 20 --led-rows=16 --led-chain=3 --led-show-refresh --led-pwm-lsb-nanoseconds 100 -D 1 /home/pi/images/dumpsterfire.ppm
+// // sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/demo -t 20 --led-rows=16 --led-chain=3 --led-show-refresh --led-pwm-lsb-nanoseconds 100 -D 1 /home/pi/images/dumpsterfire.ppm
 
-// try:
+// // try:
 
-// sudo apt-get remove bluez bluez-firmware pi-bluetooth triggerhappy pigpio
+// // sudo apt-get remove bluez bluez-firmware pi-bluetooth triggerhappy pigpio
 
-// --led-pwm-bits=<1..11>    : PWM bits (Default: 11).
+// // --led-pwm-bits=<1..11>    : PWM bits (Default: 11).
 
-// --led-slowdown-gpio=2   : Slowdown GPIO. Needed for faster Pis and/or slower panels (Default: 1).
+// // --led-slowdown-gpio=2   : Slowdown GPIO. Needed for faster Pis and/or slower panels (Default: 1).
+
+// sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/scrolling-text-example --led-chain=3 -f /home/pi/rpi-rgb-led-matrix/fonts/8x13.bdf  -l 3 -y 9
+
+// -f /home/pi/rpi-rgb-led-matrix/fonts/8x13.bdf // abs path to font
+// -l 3 //loops
+// -x // origin
+// -y 9 // origin
+// -C 255,0,0 //color
+// -B 0,0,255 // background
+// -O 0,0,0 // outline color
+// -S 1 //spacing
